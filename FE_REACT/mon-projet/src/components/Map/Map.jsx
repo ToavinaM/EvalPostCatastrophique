a63347407975a7341105574e932416d69f01850c
ToavinaDev <80760102+ToavinaM@users.noreply.github.com>
2025-03-29 21:29:49 +0300

    [FE] leaflet-draw

--- FE_REACT/mon-projet/src/components/Map/Map.jsx
+++ FE_REACT/mon-projet/src/components/Map/Map.jsx
@@ -1,5 +1,8 @@
 import { MapContainer, TileLayer, Marker, Popup, LayersControl } from 'react-leaflet';
+import { FeatureGroup } from 'react-leaflet';
+import { EditControl } from 'react-leaflet-draw';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
 import './Map.scss';
 
 const { BaseLayer } = LayersControl;
@@ -7,20 +10,26 @@ const { BaseLayer } = LayersControl;
 const Map = () => {
     const position = [-18.918175169505677, 47.52107863993858]; // Coordonnées
 
+    const handleDrawCreated = (e) => {
+        const { layerType, layer } = e;
+        if (layerType === "polygon") {
+            const coordinates = layer.getLatLngs();
+            console.log("Coordonnées du polygone :", coordinates);
+        }
+    };
+
     return (
         <div className="map-container">
             <MapContainer center={position} zoom={13} style={{ height: "100%", width: "100%" }}>
                 <LayersControl position="topright">
-                    {/* Vue Carte Standard (OpenStreetMap) */}
                     <BaseLayer checked name="Carte">
                         <TileLayer
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            maxZoom={19} // Augmente la limite de zoom
+                            maxZoom={19}
                         />
                     </BaseLayer>
 
-                    {/* Vue Satellite (Google Maps) */}
                     <BaseLayer name="Satellite">
                         <TileLayer
                             url="https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
@@ -29,6 +38,22 @@ const Map = () => {
                         />
                     </BaseLayer>
                 </LayersControl>
+
+                {/* Groupe pour le dessin */}
+                <FeatureGroup>
+                    <EditControl
+                        position="topright"
+                        onCreated={handleDrawCreated}
+                        draw={{
+                            rectangle: false,
+                            circle: false,
+                            circlemarker: false,
+                            polyline: false,
+                            polygon: true, // Activer uniquement le dessin de polygones
+                            marker: false
+                        }}
+                    />
+                </FeatureGroup>
 
                 <Marker position={position}>
                     <Popup>
